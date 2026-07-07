54aea7d8fa4cde0e99ae376f779771a73a1a5b9e
Rafael Mendez <rafaelmendzg@gmail.com>
2026-07-07 19:56:32 -0400

    test(crypto): ajustes en las correciones unitarias

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    enviroment: 'jsdom',
-    setupFiles: './src/setupTests.js',
-
+    environment: 'jsdom',
+    setupFiles: './src/test/setupTests.js', // Ruta actualizada
   },
 });
